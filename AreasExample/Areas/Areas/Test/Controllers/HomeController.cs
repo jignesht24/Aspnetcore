a0a7ff8ea5186674321d860dfeaf58b011283a9f
Jignesh Trivedi <jignesht24@gmail.com>
2017-08-19 16:12:00 +0530

    Area example

---
+++ AreasExample/Areas/Areas/Test/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿namespace AreasExample.Areas.Test.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    [Area("Test")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AreasExample/Areas/Areas/Test/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AreasExample.Areas.Test.Models
+{
+    public class User
+    {
+    }
+}
