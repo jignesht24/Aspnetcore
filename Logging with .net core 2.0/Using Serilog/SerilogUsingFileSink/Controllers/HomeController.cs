526b13e59fba62944bdc0a1b41c89f9aa2736a6b
Jignesh Trivedi <jignesht24@gmail.com>
2018-03-21 13:40:25 +0530

    serilog file sink

---
+++ Logging with .net core 2.0/Using Serilog/SerilogUsingFileSink/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using SerilogDemo.Models;
+
+namespace SerilogDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public HomeController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            Log.Information("Index method called!!!");
+            return View();
+        }
+    
+    }
+}
---
+++ Logging with .net core 2.0/Using Serilog/SerilogUsingFileSink/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace SerilogDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateFileLogger();
+            CreateFileLoggerUsingJSONFile();
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+
+        public static void CreateFileLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Information()
+                            .MinimumLevel.Override("SerilogDemo", LogEventLevel.Information)
+                            .WriteTo.File("Logs/Example.txt",
+                                    LogEventLevel.Information, // Minimum Log level
+                                    rollingInterval: RollingInterval.Day, // This will append time period to the filename like Example20180316.txt
+                                    retainedFileCountLimit: null,
+                                    fileSizeLimitBytes: null,
+                                    outputTemplate: "{Timestamp:dd-MMM-yyyy HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",  // Set custom file format
+                                    shared: true // Shared between multi-process shared log files
+                                    )
+                            .CreateLogger();
+        }
+
+        private static void CreateFileLoggerUsingJSONFile()
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+    }
+}
